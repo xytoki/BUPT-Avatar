a50e2714a23f14455ab669401fb3cf081f5e458d
xytoki <7547189+xytoki@users.noreply.github.com>
2019-09-25 11:09:51 +0800

    add heads

---
+++ src/heads.js
@@ -0,0 +1,21 @@
+
+import head1 from '@/assets/head/head1.png'
+import head2 from '@/assets/head/head2.png'
+import head3 from '@/assets/head/head3.png'
+export default [
+    {
+        src:head1,
+        preview:head1,
+        params:[0,0,512,512]
+    },
+    {
+        src:head2,
+        preview:head2,
+        params:[0,0,512,512]
+    },
+    {
+        src:head3,
+        preview:head3,
+        params:[0,0,512,512]
+    }
+]
