3c2b4d2ea7d859896dea4e80a317d49ac98eac2b
xytoki <7547189+xytoki@users.noreply.github.com>
2019-09-27 21:24:01 +0800

    add more image

--- src/heads.js
+++ src/heads.js
@@ -2,7 +2,11 @@
 import head1 from '@/assets/head/head1.png'
 import head2 from '@/assets/head/head2.png'
 import head3 from '@/assets/head/head3.png'
+import b1 from '@/assets/head/b1.png'
+import b2 from '@/assets/head/b2.png'
+import b3 from '@/assets/head/b3.png'
 import n1 from '@/assets/head/n1.png'
+import n2 from '@/assets/head/n2.png'
 export var heads =[
     {
         src:head1,
@@ -18,6 +22,21 @@ export var heads =[
         src:head3,
         preview:head3,
         params:[0,0,512,512]
+    },
+    {
+        src:b1,
+        preview:b1,
+        params:[0,0,512,512]
+    },
+    {
+        src:b2,
+        preview:b2,
+        params:[0,0,512,512]
+    },
+    {
+        src:b3,
+        preview:b3,
+        params:[0,0,512,512]
     }
 ]
 export var icons =[
@@ -26,5 +45,11 @@ export var icons =[
         preview:n1,
         x:350,
         y:350
+    },
+    {
+        src:n2,
+        preview:n2,
+        x:250,
+        y:200
     }
 ]
